5b3a7454f3e5336f2b91b6a21a64070b0e6e8d54
Max <maxnelson997@gmail.com>
2018-06-01 13:56:57 -0600

    added course selected styles

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -24,7 +24,7 @@ class Arrow extends Component {
     render() {
         this.id = `arrow-${this.props.id}`
         return (
-            <a id={this.id} onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
+            <a id={this.id} onClick={() => this.toggleArrow()} className={`${this.props.className} arrow`}></a>
         )
     }
 }
