f364a29640339adf85fb5a9191998ebf67ced179
Max <maxnelson997@gmail.com>
2018-05-31 18:04:17 -0600

    fixed grid and added arrow component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Icon from '../icon';
+import Icon from "../icon";
+import Arrow from "../arrow";
 
 class LibraryCourse extends Component {
-    render() {
-        return (
-            <div className="library-course">
-                <label className="library-course__title">Problem Solving</label>
-                { Icon('fas fa-check', 'library-course__icon') }
-                {/* arrow component */}
-                {/* action button component */}
-                <div className="library-course__description">
-                    <label>Course Description</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla facilisis mi, non pellentesque metus consectetur vel. Quisque dictum, lectus vitae dignissim tincidunt, sapien nibh placerat diam, quis blandit enim nulla in felis.</p>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="library-course">
+        <div className="library-course__title-arrow">
+          <label className="library-course__title">Problem Solving</label>
+          {Icon("fas fa-check", "library-course__icon")}
+        </div>
+        <Arrow className="library-course__arrow" />
+        {/* action button component */}
+        <div className="library-course__description">
+          <label>Course Description</label>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            fringilla facilisis mi, non pellentesque metus consectetur vel.
+            Quisque dictum, lectus vitae dignissim tincidunt, sapien nibh
+            placerat diam, quis blandit enim nulla in felis.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LibraryCourse;
