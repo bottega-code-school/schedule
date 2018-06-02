1ce6504133acd26d9c2e19c4df5720f11cbbea89
Max <maxnelson997@gmail.com>
2018-06-01 19:11:58 -0600

    overflow scrolling on library component

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,38 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import * as actions from '../../actions';
+import * as actions from "../../actions";
 
-import LibraryCourse from './libraryCourse';
+import LibraryCourse from "./libraryCourse";
 
 class Library extends Component {
+  componentWillMount() {
+    this.props.fetchCourses();
+  }
 
-    componentWillMount() {
-        this.props.fetchCourses()
-    }
+  renderCourses() {
+    const data = this.props.courses;
 
-    renderCourses() {
-        const data = this.props.courses
+    return data.map((course, index) => {
+      return <LibraryCourse {...course} key={index} />;
+    });
+  }
 
-        return data.map((course, index) => {
-            return <LibraryCourse {...course} key={index}/>
-        })
-    }
-
-    render() {
-        return (
-            <div className="library">
-                <h1 className="library__title">Course Library</h1>
-                { this.renderCourses() }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="library">
+        <div className="library__container">
+          <h1 className="library__container__title">Course Library</h1>
+          {this.renderCourses()}
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        courses: state.courses
-    }
+  return {
+    courses: state.courses
+  };
 }
 
 export default connect(mapStateToProps, actions)(Library);
