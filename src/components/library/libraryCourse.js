8ec0a20be2000cd6f67301c50ea2f4c3a30bff47
Max <maxnelson997@gmail.com>
2018-06-01 19:26:53 -0600

    rendering checkmark and add remove button based on enrollment status

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -37,7 +37,7 @@ class LibraryCourse extends Component {
       <div id={this.id} className="library-course">
         <div className="library-course__title-check">
           <div className="library-course__title">{this.props.title}</div>
-          {Icon("fas fa-check", "library-course__icon")}
+          { this.props.enrolled ? Icon("fas fa-check", "library-course__icon") : ''}
         </div>
         <Arrow
           callback={status => this.handleCallback(status)}
@@ -47,7 +47,7 @@ class LibraryCourse extends Component {
         <Action
           id={this.props.id}
           onClick={() => this.props.toggleEnrolled(this.props.id)}
-          className="library-course__action"
+          className={`library-course__action ${this.props.enrolled ? 'action-remove' : ''}`}
         />
         <AnimateHeight
           duration={300}
